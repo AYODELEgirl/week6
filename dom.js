902ded2869f449695e03db907e091f175a45516f
AYODELEgirl <timimarvel@gmail.com>
2024-01-09 10:26:32 -0800

    new commit

---
+++ dom.js
@@ -0,0 +1,38 @@
+// QUERY SELECTOR
+const title = document.querySelector('h2')
+title.textContent = 'DOM Manipulations'
+
+
+const para = document.querySelector('p')
+const para2 = document.querySelector('p:nth-of-type(2')
+const para3 = document.querySelector('body>:nth-child(5)')
+const para4 = document.querySelector('p+p+p+p')
+const para5 = document.querySelector('.last')
+
+
+para.textContent = 'this is the first paragraph'
+para2.textContent = 'this is the second paragraph'
+para3.textContent = 'this is the third paragraph'
+para4.textContent = 'this is the fourth paragraph'
+para5.textContent = 'this is the last paragraph'
+
+// querySelectorAll
+const lists = document.querySelectorAll('ul li')
+lists[0].textContent = 'list 001'
+lists[1].textContent = 'list 002'
+lists[2].textContent = 'list 003'
+lists[3].textContent = 'list 004'
+lists[4].textContent = 'list 005'
+lists[5].textContent = 'list 006'
+
+
+const unique = document.getElementById('one')
+// unique.style.backgroundColor = '#E55'
+unique.style.textTransform = 'uppercase'
+unique.style.borderBottom = '6px dotted blue'
+
+// getElementByClassName
+
+const classOne = document.getElementsByClassName('one')
+classOne[1].textContent = 'this is a class paragraph'
+// this getelementbyclass behaves like queryselector all....which means we need to add index,so using the [1] will apply the styling to the the second element thet has the class one
---
+++ script.js
@@ -0,0 +1,23 @@
+let firstName = prompt('what is your first name')
+// firstName = 'marvellous fawumi'
+alert (firstName)
+let lastName = 'fawumi'
+
+const town = `lagos`
+alert(town)
+
+ 
+
+const age = 50
+
+// let statement = 'my name is ' + firstName + ' and my surname is'+ lastName + ' and i live in' + town
+let statement = `my name is ${firstName} and my surame is ${lastName} and i live in ${town}. I was borm in ${new Date() .getFullYear()-age}`
+
+console.log(statement)
+// const year = 2024
+// const year2 = 2024 - 1
+// // alert('marvel')
+// console.log(marvel)
+
+// const _ = 600
+// alert(_) 
