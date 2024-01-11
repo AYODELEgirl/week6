58d003da5cc54e69716cd4281fbea34a02c3f863
AYODELEgirl <timimarvel@gmail.com>
2024-01-11 10:47:02 -0800

    new commit

--- dom.js
+++ dom.js
@@ -3,25 +3,26 @@ const title = document.querySelector('h2')
 title.textContent = 'DOM Manipulations'
 
 
-const para = document.querySelector('p')
+const para1 = document.querySelector('p')
 const para2 = document.querySelector('p:nth-of-type(2')
 const para3 = document.querySelector('body>:nth-child(5)')
 const para4 = document.querySelector('p+p+p+p')
 const para5 = document.querySelector('.last')
 
 
-para.textContent = 'this is the first paragraph'
+para1.textContent = 'this is the first paragraph'
 para2.textContent = 'this is the second paragraph'
 para3.textContent = 'this is the third paragraph'
 para4.textContent = 'this is the fourth paragraph'
-para5.textContent = 'this is the last paragraph'
+para5.textContent = 'this is the fifth paragraph'
+
 
 // querySelectorAll
-const lists = document.querySelectorAll('ul li')
+const lists = document.querySelectorAll('li')
 lists[0].textContent = 'list 001'
 lists[1].textContent = 'list 002'
 lists[2].textContent = 'list 003'
-lists[3].textContent = 'list 004'
+lists[3].textContent = 'list 004'  
 lists[4].textContent = 'list 005'
 lists[5].textContent = 'list 006'
 
@@ -35,4 +36,15 @@ unique.style.borderBottom = '6px dotted blue'
 
 const classOne = document.getElementsByClassName('one')
 classOne[1].textContent = 'this is a class paragraph'
+
+let div1 = document.getElementsByTagName('p')
+div1[0].style.backgroundColor = 'green'
+div1[1].textContent = ('marvelous')
+div1[1].style.textAlign = 'right'
+lists[2].textContent = 'marvel'
+
+const par = document.querySelector('#parlo')
+par.innerHTML = `I'm a <br> <strong> frontend </strong> developer. He said <q> I am coming </q>`
+
+
 // this getelementbyclass behaves like queryselector all....which means we need to add index,so using the [1] will apply the styling to the the second element thet has the class one
--- script.js
+++ script.js
@@ -6,6 +6,10 @@ let lastName = 'fawumi'
 const town = `lagos`
 alert(town)
 
+const time = 530
+const realTime = time - 3
+console.log(realTime)
+
  
 
 const age = 50
@@ -14,10 +18,22 @@ const age = 50
 let statement = `my name is ${firstName} and my surame is ${lastName} and i live in ${town}. I was borm in ${new Date() .getFullYear()-age}`
 
 console.log(statement)
-// const year = 2024
+
+let firstParagraph = document.getElementById('div1')
+let allParagraph = firstParagraph.getElementsByTagName('p')
+
+
+
+// alert(`there are ${allParagraph.length} pargraphs in div1 `)
+
+
+
+
+//  const year = 2024
 // const year2 = 2024 - 1
 // // alert('marvel')
 // console.log(marvel)
 
 // const _ = 600
 // alert(_) 
+
