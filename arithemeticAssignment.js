1498ecd9dee946082ada0021ea0912c6dbf1d70e
AYODELEgirl <timimarvel@gmail.com>
2024-01-14 10:25:00 -0800

    new commit

---
+++ arithemeticAssignment.js
@@ -0,0 +1,9 @@
+let num1 = 15
+let num2 = 30
+let num3 = 10
+
+console.log(`The addition of num1 and num2 is ${ num1 + num2}`)
+console.log(`The subtraction of num2 from num3 is ${num2 - num3}`)
+console.log(`The multiplication of num1 and num2 is ${num1 * num2}`)
+console.log(`Num3 divided by num2 is ${num3 / num2}`)
+console.log(`Num1 modulo num2 is ${num1 % num2}`)
