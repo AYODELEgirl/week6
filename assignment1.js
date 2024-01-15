9d3b4ee52f12ceb762c81f70ae55b76e5ee1f764
AYODELEgirl <timimarvel@gmail.com>
2024-01-15 13:23:34 -0800

    feat : add assignment

--- assignment1.js
+++ assignment1.js
@@ -4,9 +4,13 @@ const num2 = 30
 const num3 = 10
 
 console.log(`The addition of num1 and num2 is ${ num1 + num2}`)
+
 console.log(`The subtraction of num2 from num3 is ${num2 - num3}`)
+
 console.log(`The multiplication of num1 and num2 is ${num1 * num2}`)
+
 console.log(`Num3 divided by num2 is ${num3 / num2}`)
+
 console.log(`Num1 modulo num2 is ${num1 % num2}`)
 
 // string manipulation
@@ -14,13 +18,20 @@ console.log(`Num1 modulo num2 is ${num1 % num2}`)
 let firstName = 'Marvellous'
 
 console.log(firstName.length)
+
 console.log(firstName.toUpperCase())
-console.log(firstName.toUpperCase())
+
 console.log(firstName.charAt(4))
+
 console.log(firstName.substring(0, 6))
+
 console.log(firstName.replace('v', 'b'))
+
 console.log(firstName.indexOf('M'))
+
 const greetings = `You\'re welcome ${firstName}`
+
 console.log(greetings)
+
 console.log(firstName > greetings)
 
